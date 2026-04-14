5619d633bb485b51a7e87c989f1339fe7fddc664
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2026-04-14 17:45:24 +0530

    feat: integrate routing and enhance UI components with theming support

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -62,7 +62,6 @@ const RepositoryList = () => {
             data={repositories}
             ItemSeparatorComponent={ItemSeparator}
             renderItem={({ item }) => <RepositoryItem item={item} />}
-        // other props
         />
     );
 };
